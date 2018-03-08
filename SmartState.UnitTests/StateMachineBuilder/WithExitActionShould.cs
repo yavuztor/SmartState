7a6ec1b7d4e4a0e36cff132c41be12d7f0605cc5
Yavuz Tor <yavuztor79@gmail.com>
2018-03-08 04:16:19 -0600

    Forgotten test body.

---
+++ SmartState.UnitTests/StateMachineBuilder/WithExitActionShould.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using SmartState.Builder;
+
+namespace SmartState.UnitTests.StateMachineBuilder
+{
+    public class WithExitActionShould
+    {
+        [Fact]
+        public void ReturnAStateBuilder()
+        {
+            // Arrange
+            // Act
+            var machine = StateMachine<SampleStates, SampleTriggers>.OnInitialState(SampleStates.Draft)
+                .Triggering(SampleTriggers.Submit).TransitionsTo(SampleStates.Submitted)
+                .OnState(SampleStates.Submitted)
+                    .WithExitAction<SampleStateful>(a => a.ExitActionCalled = true);
+
+            // Assert
+            Assert.True(machine is IBuildState<SampleStates, SampleTriggers>);
+        }
+    }
+}
